5b62deb6ba74761f21d1072a5135f87a9cc7ce9a
Alex Shkil <alexhlins@gmail.com>
2021-11-04 19:41:02 +0300

    stArt from scratch

--- main.py
+++ main.py
@@ -1,36 +1,29 @@
-import sys
 import os
-from flask import Flask, request
-from bot import botToken, hlinsBot
 import telebot
+from flask import Flask, request
+from bot import botToken
 
-TOKEN_FILE_NAME ='bot.token'
-TOKEN = botToken(TOKEN_FILE_NAME).getToken()
+TOKEN = botToken('bot.token').getToken()
+bot = telebot.TeleBot(token=TOKEN)
+server = Flask(__name__)
+bot_url = os.environ.get('key_2', 'https://hsetelebot.herokuapp.com/')
 
-bot_server = Flask(__name__)
-bot = telebot.TeleBot(TOKEN)
-
-@bot.message_handler(unc=lambda m: True, content_types=['audio', 'photo', 'voice', 'video', 'document',
-                                                                   'text', 'location', 'contact', 'sticker'])
+    
+# Если строка на входе непустая, то бот повторит ее
+@bot.message_handler(func=lambda msg: msg.text is not None)
 def reply_to_message(message):
     bot.send_message(message.chat.id, message.text)
 
-
-@bot_server.route('/' + TOKEN, methods=['POST'])
+@server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
 
-@bot_server.route("/")
+@server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url=os.environ.get('key_2', 'https://hsetelebot.herokuapp.com/') + TOKEN) #
+    bot.set_webhook(url=bot_url + TOKEN) #
     return "!", 200
 
-
-
-
-if __name__ == '__main__':
-    host = "0.0.0.0"
-    port=int(os.environ.get('key_3', 5001))
-    bot_server.run(host=host, port=port)
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
