119bd04e5154518522bc13cbb733f2068dec6325
Alex Shkil <alexhlins@gmail.com>
2021-11-05 22:36:34 +0300

    fixed port getter

--- main.py
+++ main.py
@@ -27,4 +27,4 @@ def webhook():
     return "!", 200
 
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('key_3', 5000)))
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
